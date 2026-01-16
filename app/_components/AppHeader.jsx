8a0bf356c29ad893e6ae08e06cd2a67c42a4700e
Kaloyan <kaloyanbobev@gmail.com>
2026-01-16 12:18:16 +0000

    create and implement app header

---
+++ app/_components/AppHeader.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import {SidebarTrigger} from  '@/components/ui/sidebar'
+
+function AppHeader() {
+  return (
+    <div className='p-3'>
+         <SidebarTrigger />
+    </div>
+  )
+}
+
+export default AppHeader
