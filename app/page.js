d803a44ec7f3961c3574414b4b4295752859e014
Kaloyan <kaloyanbobev@gmail.com>
2026-01-28 12:27:21 +0000

    create a chat input box

--- app/page.js
+++ app/page.js
@@ -3,9 +3,14 @@
 import { Button } from "@/components/ui/button";
 import { useTheme } from "next-themes";
 import Image from "next/image";
+import ChatInputBox from "./_components/ChatInputBox";
 
 export default function Home() {
   const { setTheme } = useTheme();
 
-  return <div></div>;
+  return (
+    <div>
+      <ChatInputBox />
+    </div>
+  );
 }
