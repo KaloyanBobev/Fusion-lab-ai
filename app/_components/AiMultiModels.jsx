1ab5c6ad4464bb27f7346541bc776529466749a4
Kaloyan <kaloyanbobev@gmail.com>
2026-02-02 21:51:18 +0000

    Added Layout and Multimodel

--- app/_components/AiMultiModels.jsx
+++ app/_components/AiMultiModels.jsx
@@ -11,21 +11,27 @@ import {
   SelectValue,
 } from "@/components/ui/select";
 import { Switch } from '@/components/ui/switch';
+import { Lock, MessageSquare } from "lucide-react";
+import { Button } from "@/components/ui/button";
 
 function AiMultiModels() {
   const [aiModelList, setAiModelList] = useState(AiIModelList);
 
-  const onToggleChange=(model,value)=>{
-
-  }
+  const onToggleChange = (model, value) => {
+    setAiModelList((prev) =>
+      prev.map((m) => (m.model === model ? { ...m, enable: value } : m))
+    );
+  };
   return (
     <div className="flex flex-1 has-[75vh] border-b">
       {aiModelList.map((model, index) => (
-        <div className="flex flex-col border-r h-full   min-w-[400px]">
-          <div
-            key={index}
-            className="flex w-full items-center justify-between border-b p-4"
-          >
+        <div
+          key={index}
+          className={`flex flex-col border-r h-full ${
+            model.enable ? `flex-1 min-w-[400px]` : `w-[100px] flex-none`
+          }`}
+        >
+          <div className="flex w-full h-[70px] items-center justify-between border-b p-4">
             <div className="flex items-center gap-4">
               <Image
                 src={model.icon}
@@ -34,26 +40,37 @@ function AiMultiModels() {
                 height={24}
               />
 
-              <Select>
-                <SelectTrigger className="w-[180px]">
-                  <SelectValue placeholder={model.subModel[0].name} />
-                </SelectTrigger>
-                <SelectContent>
-                  {model.subModel.map((subModel, index) => (
-                    <SelectItem key={index} value={subModel.name}>
-                      {subModel.name}
-                    </SelectItem>
-                  ))}
-                </SelectContent>
-              </Select>
+              {model.enable && (
+                <Select>
+                  <SelectTrigger className="w-[180px]">
+                    <SelectValue placeholder={model.subModel[0].name} />
+                  </SelectTrigger>
+                  <SelectContent>
+                    {model.subModel.map((subModel, index) => (
+                      <SelectItem key={index} value={subModel.name}>
+                        {subModel.name}
+                      </SelectItem>
+                    ))}
+                  </SelectContent>
+                </Select>
+              )}
             </div>
             <div>
-              <Switch
-                checked={model.enable}
-                OnCheckedChanhe={(v) => onToggleChange()}
-              />
+              {model.enable ? (
+                <Switch
+                  checked={model.enable}
+                  onCheckedChange={(v) => onToggleChange(model.model, v)}
+                />
+              ) : (
+                <MessageSquare
+                  onClick={() => onToggleChange(model.model, true)}
+                />
+              )}
             </div>
           </div>
+          {model.premium&&model.enable&& <div className='flex items-center justify-center h-full'>
+            <Button><Lock/> Upgrade to unlock</Button>
+          </div>}
         </div>
       ))}
     </div>
