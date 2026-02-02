806f1fff22e6c033edb339806fc76a60f72fd87f
Kaloyan <kaloyanbobev@gmail.com>
2026-02-02 19:34:43 +0000

    create a field for each of the ai models

--- app/_components/AiMultiModels.jsx
+++ app/_components/AiMultiModels.jsx
@@ -14,28 +14,45 @@ import { Switch } from '@/components/ui/switch';
 
 function AiMultiModels() {
   const [aiModelList, setAiModelList] = useState(AiIModelList);
+
+  const onToggleChange=(model,value)=>{
+
+  }
   return (
     <div className="flex flex-1 has-[75vh] border-b">
       {aiModelList.map((model, index) => (
-        <div key={index} className="flex w-full items-center justify-between">
-          <div className="flex items-center gap-4">
-            <Image src={model.icon} alt={model.model} width={24} height={24} />
+        <div className="flex flex-col border-r h-full   min-w-[400px]">
+          <div
+            key={index}
+            className="flex w-full items-center justify-between border-b p-4"
+          >
+            <div className="flex items-center gap-4">
+              <Image
+                src={model.icon}
+                alt={model.model}
+                width={24}
+                height={24}
+              />
 
-            <Select>
-              <SelectTrigger className="w-[180px]">
-                <SelectValue placeholder={model.subModel[0].name} />
-              </SelectTrigger>
-              <SelectContent>
-                {model.subModel.map((subModel, index) => (
-                  <SelectItem key={index} value={subModel.name}>
-                    {subModel.name}
-                  </SelectItem>
-                ))}
-              </SelectContent>
-            </Select>
-          </div>
-          <div>
-            <Switch />
+              <Select>
+                <SelectTrigger className="w-[180px]">
+                  <SelectValue placeholder={model.subModel[0].name} />
+                </SelectTrigger>
+                <SelectContent>
+                  {model.subModel.map((subModel, index) => (
+                    <SelectItem key={index} value={subModel.name}>
+                      {subModel.name}
+                    </SelectItem>
+                  ))}
+                </SelectContent>
+              </Select>
+            </div>
+            <div>
+              <Switch
+                checked={model.enable}
+                OnCheckedChanhe={(v) => onToggleChange()}
+              />
+            </div>
           </div>
         </div>
       ))}
