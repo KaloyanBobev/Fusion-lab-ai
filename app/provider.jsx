755a239e6e4803276660eb7b707fc9aa17071085
Kaloyan <kaloyanbobev@gmail.com>
2026-02-07 11:41:12 +0000

    create a user chack and if new user fill the user data

--- app/provider.jsx
+++ app/provider.jsx
@@ -6,13 +6,32 @@ import { AppSidebar } from "./_components/AppSidebar";
 import AppHeader from "./_components/AppHeader";
 import { useUser } from "@clerk/nextjs";
 import { db } from "@/config/FirebaseConfig";
-
+import { getDoc, setDoc } from "firebase/firestore";
 
 function Provider({ children, ...props }) {
   const { user } = useUser();
 
-  const CrateNewuser = () => {};
-  const userRef=doc(db,"users",user?.primaryEmailAddress?.emailAddress);
+  const CrateNewUser = async () => {
+    //if user exist
+    const userRef = doc(db, "users", user?.primaryEmailAddress?.emailAddress);
+    const userSnap = await getDoc(userRef);
+    if (userSnap.exists()) {
+      console.log("Existing user");
+      return;
+    } else {
+      const userData = {
+        name: user?.fullName,
+        email: user?.primaryEmailAddress?.emailAddress,
+        crearAT: new Date(),
+        ramingMsg: 5, //only for free users
+        plan: "Free",
+        credits: 1000, //paid users
+      };
+      await setDoc(userRef, userData);
+      console.log("New User data saved");
+    }
+    //if not then insert
+  };
   return (
     <NextThemesProvider
       attribute="class"
