d6f074e78e722854f461573b809cf7a93d98e030
Kaloyan <kaloyanbobev@gmail.com>
2026-05-09 23:09:28 +0100

    fix the problem with crating a chat wrapper

---
+++ app/ChatWrapper.jsx
@@ -0,0 +1,7 @@
+"use client";
+
+import ChatInputBox from "./_components/ChatInputBox";
+
+export default function ChatWrapper() {
+  return <ChatInputBox />;
+}
--- app/page.js
+++ app/page.js
@@ -1,15 +1,26 @@
-"use client";
-export const dynamic = "force-dynamic";
-import { useTheme } from "next-themes";
+import { Suspense } from "react";
+import ChatWrapper from "./ChatWrapper";
 
-import ChatInputBox from "./_components/ChatInputBox";
-
-export default function Home() {
-  const { setTheme } = useTheme();
-
+export default function Page() {
   return (
-    <div>
-      <ChatInputBox />
-    </div>
+    <Suspense fallback={<div>Loading...</div>}>
+      <ChatWrapper />
+    </Suspense>
   );
 }
+
+// "use client";
+// export const dynamic = "force-dynamic";
+// import { useTheme } from "next-themes";
+
+// import ChatInputBox from "./_components/ChatInputBox";
+
+// export default function Home() {
+//   const { setTheme } = useTheme();
+
+//   return (
+//     <div>
+//       <ChatInputBox />
+//     </div>
+//   );
+// }
