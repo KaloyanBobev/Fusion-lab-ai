6558b5407bfb26af413e0a7e1da3c3ae94436494
Kaloyan <kaloyanbobev@gmail.com>
2026-05-09 23:12:17 +0100

    Refactor Home component to Page and use Suspense

--- app/page.js
+++ app/page.js
@@ -1,16 +1,10 @@
-"use client";
-export const dynamic = "force-dynamic";
+import { Suspense } from "react";
+import ChatWrapper from "./ChatWrapper";
 
-import { useTheme } from "next-themes";
-
-import ChatInputBox from "./_components/ChatInputBox";
-
-export default function Home() {
-  const { setTheme } = useTheme();
-
+export default function Page() {
   return (
-    <div>
-      <ChatInputBox />
-    </div>
+    <Suspense fallback={<div>Loading...</div>}>
+      <ChatWrapper />
+    </Suspense>
   );
 }
